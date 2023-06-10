cfb44deb703c51bee03c085149288da41cb69c76
Weibo Zhao <94233933+bolecodex@users.noreply.github.com>
2023-06-10 12:37:16 +0200

    Update step-01-k8s-packages-ubuntu.sh

--- terraform-k8s-kubeadm-ec2-cluster/kubeadm-scripts/step-01-k8s-packages-ubuntu.sh
+++ terraform-k8s-kubeadm-ec2-cluster/kubeadm-scripts/step-01-k8s-packages-ubuntu.sh
@@ -60,6 +60,7 @@ version = 2
 	TOML
 
 	# Restart containerd
+	sudo apt-get install -y containerd
 	sudo systemctl restart containerd	
 fi
 
@@ -69,12 +70,11 @@ fi
 
 # this script supports Ubuntu 20.04 LTS and later only
 # run this script with sudo
-
-if ! [ $USER = root ]
-then
-	echo run this script with sudo
-	exit 3
-fi
+# if ! [ $USER = root ]
+# then
+# 	echo run this script with sudo
+# 	exit 3
+# fi
 
 # setting MYOS variable
 MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
@@ -96,18 +96,19 @@ EOF
 	sudo apt-get update
 	sudo apt-get install -y kubelet kubeadm kubectl
 	sudo apt-mark hold kubelet kubeadm kubectl
-	swapoff -a
+	sudo swapoff -a
 	
-	sed -i 's/\/swap/#\/swap/' /etc/fstab
+	sudo sed -i 's/\/swap/#\/swap/' /etc/fstab
 fi
 
 # Set iptables bridging
-cat <<EOF >  /etc/sysctl.d/k8s.conf
+cat <<EOF > sudo /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sysctl --system
+sudo sysctl --system
 
 sudo crictl config --set \
     runtime-endpoint=unix:///run/containerd/containerd.sock
 echo 'after initializing the control node, follow instructions and use kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml to install the calico plugin (control node only). On the worker nodes, use sudo kubeadm join ... to join'
+exit 0
