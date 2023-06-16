b14bda726b2c3daac12f6eef879c96bb5ae4cbd4
Weibo Zhao <94233933+bolecodex@users.noreply.github.com>
2023-06-17 01:08:02 +0200

    Update outputs.tf

--- terraform-k8s-kubeadm-ec2-cluster/outputs.tf
+++ terraform-k8s-kubeadm-ec2-cluster/outputs.tf
@@ -1,7 +1,7 @@
 output "node_dns_info" {
   value = [
-    for ec2 in concat([aws_instance.ec2_instance_control_plane_node], aws_instance.ec2_instance_worker_node) : {
-      ssh_cmd = ec2.tags["Name"] == "k8s-control-plane-node" ? "${ec2.tags["Name"]} : ssh -i ${local.pem_file} ubuntu@${ec2.public_dns} ; Run 'kubectl get nodes' to test cluster is up!" : "${ec2.tags["Name"]} : ssh -i ${local.pem_file} ubuntu@${ec2.public_dns}"
+    for ec2 in concat([aws_instance.ec2_instance_control_plane_node], [aws_instance.ec2_instance_worker_node1], [aws_instance.ec2_instance_worker_node2]) : {
+      ssh_cmd = ec2.tags["Name"] == "k8s-control-plane-node" ? "${ec2.tags["Name"]} : ssh -i ${local.pem_file} ubuntu@${ec2.public_ip} ; Run 'kubectl get nodes' to test cluster is up!" : "${ec2.tags["Name"]} : ssh -i ${local.pem_file} ubuntu@${ec2.public_ip}"
     }
   ]
   description = "Cluster Node DNS and IPs"
