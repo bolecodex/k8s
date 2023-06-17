216e0680a22752e6fddae023e7aeea46a3ac9d72
Weibo Zhao <94233933+bolecodex@users.noreply.github.com>
2023-06-17 16:13:23 +0200

    Update step-02-k8s-cp-init.sh

--- terraform-k8s-kubeadm-ec2-cluster/kubeadm-scripts/step-02-k8s-cp-init.sh
+++ terraform-k8s-kubeadm-ec2-cluster/kubeadm-scripts/step-02-k8s-cp-init.sh
@@ -1,4 +1,4 @@
-# STEP 4 - Initialize and setup Control Plane Node
+# Initialize and setup Control Plane Node
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#initializing-your-control-plane-node
 #
 # IMPORTANT - This runs on the Control Plane node
@@ -16,18 +16,7 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install the Calico Network Add-On on the Control Plane node
-# Reference: https://docs.tigera.io/calico/3.25/getting-started/kubernetes/quickstart
-# For other Network options, see
-# https://kubernetes.io/docs/concepts/cluster-administration/addons/
-
-# Install the Tigera Calico operator and custom resource definitions.
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
-
-# Install Calico by creating the necessary custom resource. 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml
-
-# FYI, install the calicoctl CLI tool to manage Calico resources and perform administrative functions following the link:
-# https://docs.tigera.io/calico/3.25/operations/calicoctl/install
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
 
 # Test Control Plane node. Expected to be in Ready state this time
 kubectl get nodes
